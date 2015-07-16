4038b411164ebfed3baae8a5f4688c3dc3992abf
古俊杰 <dggugjj@163.com>
2015-07-16 17:21:49 +0800

    初始化

---
+++ AddressComponent.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dggug
+ * Date: 2015/7/16
+ * Time: 17:16
+ */
+
+namespace api\jdjos;
+
+use yii\base\Component;
+
+class AddressComponent extends Component
+{
+
+}
